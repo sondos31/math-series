192f76a2bf069c840f3b276a4600bc8e5e51c72c
sondos31 <sondos31@gmail.com>
2022-06-01 18:42:57 +0300

    dd

---
+++ tests/test_series.py
@@ -0,0 +1,11 @@
+import math-series
+import test_series
+#fibunaccie test
+   def test_fib_zero():
+   def test_fib_one():
+
+
+   def test_fib_two():
+   def test_fib_three():
+   def test_fib_four():
+   #return
